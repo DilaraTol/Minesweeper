19425ccc408741838e071306dab19f745ca1d431
Dilara Tolegen <dilaratolegen@gmail.com>
2026-03-07 19:02:58 +0000

    1

--- minesweeper.py
+++ minesweeper.py
@@ -102,30 +102,37 @@ class Sentence():
         return f"{self.cells} = {self.count}"
 
     def known_mines(self):
+        if len(self.cells) == self.count and self.count != 0:
+            return set(self.cells)
+        return set()
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return set(self.cells)
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1 
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
